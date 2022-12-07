70147c8fe5abfe9cd5b0121a3465120eaabefead
Valentino <valentino.giudice@network.elancoah.com>
2022-12-07 15:45:08 +0100

    refactor: e2e test

--- client/cypress/e2e/todo.cy.js
+++ client/cypress/e2e/todo.cy.js
@@ -6,8 +6,8 @@ describe("todos", () => {
   it("adds, completes and deletes a todo", () => {
     cy.findByRole("textbox").type("Football").type("{enter}");
     cy.findByText(/football/i).should("exist");
-    cy.get('[type="checkbox"]').check();
-    cy.get('[type="checkbox"]').should("be.checked");
+    cy.findByRole("checkbox").click();
+    cy.findByRole("checkbox").should("be.checked");
     cy.findByRole("button", { name: /delete/i }).click();
     cy.findByText(/football/i).should("not.exist");
   });
