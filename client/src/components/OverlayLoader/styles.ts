447477c3c33d45f0d4ed8fd2867604ad8359d8c4
Valentino <valentino.giudice@network.elancoah.com>
2022-12-11 18:59:23 +0100

    style: set overlay loader fix height

--- client/src/components/OverlayLoader/styles.ts
+++ client/src/components/OverlayLoader/styles.ts
@@ -5,6 +5,9 @@ export const StyledOverlayLoader = styled(LoadingOverlay)`
   .OverlayLoader_overlay {
     background: #242424;
   }
+  &.OverlayLoader_wrapper {
+    height: calc(100vh - 200px);
+  }
   &.OverlayLoader_wrapper--active {
     overflow: hidden;
   }
